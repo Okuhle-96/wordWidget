8d613ad278620126af39c433a273a71a1badad3e
Okuhle-96 <54100625+Okuhle-96@users.noreply.github.com>
2022-03-25 01:30:05 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -14,59 +14,46 @@ let wordsWidgetInstance = wordsWidgetFactory();
 
 // MARK LONGER WORDS
 const markLongerWords = () => {
- 
-  const words = textInput.value;
-  const word = words.split(" ");
 
-  let count = 0;
-  let wordsWidget = "";
+    const words = textInput.value;
+    const word = words.split(" ");
 
-  for(let i = 0; i < word.length; i++) {
-      const wordelement = word[i];
+    let count = 0;
+    let wordsWidget = "";
 
-      if (wordelement.length > 4) {
-          wordsWidget += `<mark>${wordelement}</mark> `
-      } else {
-          wordsWidget += wordelement + " ";
-      }
-      if (wordelement.length > 0) {
-          count++
-      }
-      wordsOutput.innerHTML = wordsWidget;
-  }
-  counter.innerHTML = count;
+    for (let i = 0; i < word.length; i++) {
+        const wordelement = word[i];
+
+        if (wordelement.length > 4) {
+            wordsWidget += `<mark style="padding: 10px; background-color: goldenrod; color: white;">${wordelement}</mark> `
+        } else {
+            wordsWidget += wordelement + " ";
+        }
+        if (wordelement.length > 0) {
+            count++
+        }
+        wordsOutput.innerHTML = wordsWidget;
+    }
+    counter.innerHTML = count;
 }
 submitBtn.addEventListener('click', markLongerWords)
 
-// const markLongerWords = () => {
-//     const words = textInput.value;
-//     const word = words.split(" ");
-//     if(word){
-//         wordsWidgetInstance.markLongerWordsFactory(word);
-//     }
-//     wordsOutput.innerHTML = wordsWidgetInstance.markLongerWordsFactory(word);
-//     counter.innerHTML = wordsWidgetInstance.getTheCountFactory();
-// }
-// submitBtn.addEventListener('click', markLongerWords)
-
 // HIDE UNMARKED WORDS
 const hideUmarkedWords = () => {
-  const words = textInput.value;
-  const word = words.split(" ");
+    const words = textInput.value;
+    const word = words.split(" ");
 
-  let checkLongerWord = "";
+    let checkLongerWord = "";
 
-  for (let i = 0; i < word.length; i++) {
-      const wordelement = word[i];
 
-      if((checkbox.checked === true) && (word[i].length >= 5)) {
-          checkLongerWord += `<mark style="background-color: red; padding: 10px; color: white;>${wordelement}</mark> `
-      } else {
+    for (let i = 0; i < word.length; i++) {
+        const wordelement = word[i];
 
-          checkLongerWord += ''
-      }
-      wordsOutput.innerHTML = checkLongerWord
-  }
+        if ((checkbox.checked == true) && (wordelement.length > 4)) {
+            checkLongerWord += `<mark style="padding: 10px; background-color: darkred; color: white;">${wordelement}</mark>` + " ";
+        }
+        wordsOutput.innerHTML = checkLongerWord
+    }
 
 }
 checkWords.addEventListener('change', hideUmarkedWords)
@@ -74,19 +61,28 @@ checkWords.addEventListener('change', hideUmarkedWords)
 // GET THE LONGEST WORD
 const getTheLongestWord = () => {
 
-  const words = textInput.value;
-  const word = words.split(" ");
+    const words = textInput.value;
+    const word = words.split(" ");
 
-  let longestWord = "";
+    let longestWord = "";
+    let anotherWord = "";
 
-  for (let i = 0; i < word.length; i++) {
-      if(word[i].length >= longestWord.length) {
-          longestWord = word[i]
-      }
-  }
-  wordsOutput.innerHTML = `<mark style="background-color: green; padding: 10px; color: white;">${longestWord}</mark>`;
-  }
-  checkLongestWord.addEventListener('change', getTheLongestWord)
+    for (let i = 0; i < word.length; i++) {
+        if (word[i].length >= longestWord.length) {
+            longestWord = word[i]
+        }
+    }
+
+    for (let i = 0; i < word.length; i++) {
+        let wordIndex = word[i];
+        if (wordIndex.length === longestWord.length) {
+            anotherWord += wordIndex + " ";
+
+        }
+        wordsOutput.innerHTML = `<mark style="background-color: darkgreen; padding: 10px; font-family: monospace; margin: 10px; color: white;">${anotherWord}</mark>` + " ";
+    }
+}
+checkLongestWord.addEventListener('change', getTheLongestWord)
 
 // KEEP TRACK OF LAST 5 SENTENCES
 const keepTrackOfLastFiveSentences = () => {
